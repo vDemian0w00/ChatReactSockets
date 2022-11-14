7a71d5d72a744087f0f7b3d1cc88b4c8bdf36710
vDemian0w00 <cfajardoa2000@alumno.ipn.mx>
2022-11-14 07:32:17 -0600

    si

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -5,7 +5,7 @@ const router = Router()
 
 router.post("/", createUser);
 router.post("/login", getUser);
-
+router.get("/ping", (req, res) => res.send("pong"));
 router.get("/contacts/:id", getContacts)
 
 export default router
